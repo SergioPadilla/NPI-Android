48a2004b14b37e91a5a7942e8be5f35d03085a1d
sergiopadillalp <splsergiopadilla@gmail.com>
2016-12-22 19:40:56 +0100

    init

--- apps/app/src/main/java/com/piser/apps/ActivitySelector.java
+++ apps/app/src/main/java/com/piser/apps/ActivitySelector.java
@@ -1,13 +1,41 @@
 package com.piser.apps;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.piser.apps.AppOne.AppOne;
 
 public class ActivitySelector extends AppCompatActivity {
+
+    private FrameLayout app_one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selector);
+
+        app_one = (FrameLayout) findViewById(R.id.app_one);
+
+
+    }
+
+    private void initActivity(Class activity) {
+        startActivity(new Intent(this, activity));
+    }
+
+    private View.OnClickListener appOneListener() {
+        /**
+         * Get the action for first button
+         */
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initActivity(AppOne.class);
+            }
+        };
     }
 }
