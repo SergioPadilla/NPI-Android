a7bc0a61e0b8fac1f73167ca457ee74b2504c907
sergiopadillalp <splsergiopadilla@gmail.com>
2016-12-22 15:01:28 +0100

    create project

---
+++ apps/app/src/main/java/com/piser/apps/ActivitySelector.java
@@ -0,0 +1,13 @@
+package com.piser.apps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selector);
+    }
+}
